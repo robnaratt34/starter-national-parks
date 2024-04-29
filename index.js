c1645dd8257210b3e141cae2bc6a448b176fbce4
John <robnaratt34@gmail.com>
2024-04-29 01:31:37 -0600

    Changing text and css properties through JS

---
+++ index.js
@@ -0,0 +1,19 @@
+//Can change text of site with either innerText or innerHTML
+const descriptions = document.querySelectorAll(".description-display");
+for (let desc of descriptions.values()){
+    let content = desc.innerText;
+    if(content.length>250){
+        content = content.slice(0,250)
+        content = content + '<a href="#">...</a>';
+    }
+    desc.innerHTML = content
+}
+
+//Changes the style using the style property
+const ratings = document.querySelectorAll(".rating-display .value");
+for (let rating of ratings){
+    let ratingValue = parseFloat(rating.innerText);
+    if (ratingValue>4.7){
+        rating.style.fontWeight = "bold";
+    }
+}
